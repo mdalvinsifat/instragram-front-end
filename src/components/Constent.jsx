df2432efb148df11dcbcc8c34b667f0df1644e15
Alvin SIfat <118036905+mdalvinsifat@users.noreply.github.com>
2025-05-10 17:11:43 +0600

    Update Constent.jsx

--- src/components/Constent.jsx
+++ src/components/Constent.jsx
@@ -5,5 +5,5 @@ export const SIGNUP = "/signUp"
 export const LOGIN = '/login'
 export const PROFILE = '/profile'
 
-export const URL ="https://instragram-back-end-p.onrender.com"
-export const URLS ="https://instragram-back-end-p.onrender.com"
+export const URL ="https://instragram-back-end-git-main-alvin-sifats-projects.vercel.app"
+export const URLS ="https://instragram-back-end-git-main-alvin-sifats-projects.vercel.app"
