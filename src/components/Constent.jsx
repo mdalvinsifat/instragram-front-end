cfee444e9a3d4c0521cc67fe4c4fecd472d9a66d
Alvin SIfat <118036905+mdalvinsifat@users.noreply.github.com>
2025-05-10 15:36:25 +0600

    Update Constent.jsx

--- src/components/Constent.jsx
+++ src/components/Constent.jsx
@@ -6,3 +6,4 @@ export const LOGIN = '/login'
 export const PROFILE = '/profile'
 
 export const URL ="https://instragram-back-end-p.onrender.com"
+export const URLS ="https://instragram-back-end-p.onrender.com"
