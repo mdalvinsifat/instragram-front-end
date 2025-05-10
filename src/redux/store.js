317ead16f736ab30f545ce7c3c3665c4496a38f9
Alvin SIfat <118036905+mdalvinsifat@users.noreply.github.com>
2025-05-10 16:47:26 +0600

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -17,13 +17,13 @@ import {
   import storage from 'redux-persist/lib/storage'
 
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+  blacklist: ['socket'], // do NOT persist socketSlice
+}
 
-  const persistConfig = {
-    key: 'root',
-    version: 1,
-    storage,
-  }
-  
 
 const rootReducer = combineReducers({
     auth : AuthSlice, 
